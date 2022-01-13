3e361fcf8f1badeb7cb32973bbd0542970250ee1
laurenkch <lkoch879@gmail.com>
2022-01-13 16:13:34 -0500

    draw a card and reshuffle if you run out

--- scripts/main.js
+++ scripts/main.js
@@ -3,32 +3,58 @@
 
     let deck = [];
     let shuffledDeck = [];
-    let playerOneDeck = [];
-    let playerTwoDeck = [];
-    let playerOneWinPile = [];
-    let playerTwoWinPile = [];
 
 
-    const newgameButton = document.querySelector('.new-game');
-    newgameButton.addEventListener('click', startGame);
-    const drawButton = document.querySelector('.draw');
-    drawButton.addEventListener('click', draw);
-
-    function startGame() {
-       let player1 = new Player('you');
-        let player2 = new Player('computer');
-        console.log(player1);
-    };
-
-    function draw() {
-
-    };
+    // const newgameButton = document.querySelector('.new-game');
+    // newgameButton.addEventListener('click', startGame);
 
     //---CARDS
 
     function Card(value, suit) {
         this.value = value;
         this.suit = suit;
+    };
+
+    //-PLAYER
+
+    function Player(name, deck = [], winPile = [], cardsinPlay = []) {
+        this.name = name
+        this.deck = deck
+        this.winPile = winPile
+        this.cardsinPlay = cardsinPlay
+    };
+
+    let player1 = new Player('you');
+    let player2 = new Player('computer');
+
+console.log(player1);
+
+// function reset() {
+//         while (this.winPile > 0) {
+//             let index = (Math.floor(Math.random() * this.winPile.length));
+//             let removed = this.winPile.splice(index, 1);
+//             this.deck.push(removed);
+//         };
+//         // shuffledDeck = shuffledDeck.flat();
+//         console.log(this.deck);
+//     }
+
+ function draw(player1) {
+        console.log(this.deck.length);
+        if (this.deck.length < 2) {
+            while (this.winPile.length > 0) {
+                let index = (Math.floor(Math.random() * this.winPile.length));
+                let removed = this.winPile.splice(index, 1);
+                this.deck.push(removed);
+            };
+            this.deck = this.deck.flat();
+            console.log(this);
+        } else {
+            let activeCard = this.deck.shift();
+            console.log(this);
+            console.log(activeCard);
+            this.winPile.push(activeCard);
+        }
     };
 
     //--DECK
@@ -41,7 +67,6 @@
         };
     };
     Deck();
-    console.log(deck);
 
     //---SHUFFLE
 
@@ -53,57 +78,27 @@
         };
         shuffledDeck = shuffledDeck.flat();
     };
-
     shuffle(deck);
-    console.log(deck);
-    console.log(shuffledDeck);
 
 //--DEAL THE DECK
 
 function dealDeck(arr) {
-     playerOneDeck = arr.slice(0,(arr.length/2));
-     playerTwoDeck = arr.slice(arr.length/2,arr.length);
+     player1.deck = arr.slice(0,(arr.length/2));
+     player2.deck = arr.slice(arr.length/2,arr.length);
     }
-
-//--DRAW
 dealDeck(shuffledDeck);
 
-    console.log(playerOneDeck);
-    console.log(playerTwoDeck);
+const drawButton = document.querySelector('.draw');
+drawButton.addEventListener('click', startDraw);
 
-    function draw() {
-        let playerOneCard = playerOneDeck.shift();
-        let playerTwoCard = playerTwoDeck.shift();
-        console.log(playerOneCard);
-        console.log(playerTwoCard);
-        console.log(playerOneCard.value);
-        console.log(playerTwoCard.value);
-        if (playerOneCard.value > playerTwoCard.value) {
-            playerOneDeck.push(playerOneCard, playerTwoCard);
-        }
-    }
-    draw();
+//--DRAW
 
-    console.log(playerOneDeck);
-    console.log(playerTwoDeck);
+  function startDraw() {
+    draw.apply(player1);
+    draw.apply(player2);
 
+  };
 
-//EXTRA CODE, unsure if any will be used
-    // function Game(player1card, player2card, player1score = 0, player2score = 0) {
-    //     this.player1card = player1card
-    //     this.player2card = player2card
-    //     this.player1score = player1score
-    //     this.player2score = player2score
-    // }
-    // Game.prototype.compare = function() {
-    //     player1card > player2card ? player1score + 1: player2score +1;
-    // }
-
-    // function Player(name, deck = 0, totalcards = 0) {
-    //     this.name = name
-    //     this.deck = deck
-    //     this.totalcards = totalcards
-    // }
 
 
 

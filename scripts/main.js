7b10bf25274928e676df5a93217d510ac031f67c
laurenkch <lkoch879@gmail.com>
2022-01-13 11:37:58 -0500

    shuffle, deal and the start of draw functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 (function () {
     'use strict'
 
-    const deck = [];
-    const deck2 = [];
+    let deck = [];
+    let shuffledDeck = [];
+    let playerOneDeck = [];
+    let playerTwoDeck = [];
+    let playerOneWinPile = [];
+    let playerTwoWinPile = [];
+
 
     const newgameButton = document.querySelector('.new-game');
     newgameButton.addEventListener('click', startGame);
@@ -11,7 +16,7 @@
 
     function startGame() {
        let player1 = new Player('you');
-
+        let player2 = new Player('computer');
         console.log(player1);
     };
 
@@ -19,102 +24,86 @@
 
     };
 
-   function makeDeck() { 
-        for (let i = 2; i < 15; i++) {
-        deck.push(new Card(i,'heart'));
-        deck.push(new Card(i,'spade'));
-        deck.push(new Card(i,'diamond'));
-        deck.push(new Card(i,'club'));
-    };
-    };
-    console.log(deck);
-
-    function randomNum() {
-        (Math.floor(Math.random() * arr.length));
-    }
-
-    function shuffle(arr) {
-        let num = 0;
-        let removed = {};
-        while (arr.length > 1) 
-        num = (Math.floor(Math.random() * arr.length));
-        console.log(num);
-        removed = arr.splice(num,1);
-        console.log(removed);
-        deck2.push(removed);
-    };
-
-    shuffle(deck);
-    console.log(deck);
-    console.log(deck2);
-
-    // const Deck = {
-    //     cards: deck.length,
-    //     shuffle() {
-    //             if (deck.length === 1) return deck[0];
-    //             (Math.floor(Math.random() * this.cards));
-    //         }
-    // };
-
+    //---CARDS
 
     function Card(value, suit) {
         this.value = value;
         this.suit = suit;
     };
 
-    function Game(player1card, player2card, player1score = 0, player2score = 0) {
-        this.player1card = player1card
-        this.player2card = player2card
-        this.player1score = player1score
-        this.player2score = player2score
+    //--DECK
+    function Deck() {
+        for (let i = 2; i < 15; i++) {
+            deck.push(new Card(i, 'heart'));
+            deck.push(new Card(i, 'spade'));
+            deck.push(new Card(i, 'diamond'));
+            deck.push(new Card(i, 'club'));
+        };
+    };
+    Deck();
+    console.log(deck);
+
+    //---SHUFFLE
+
+    function shuffle(arr) {
+        while (arr.length > 0) {
+            let index = (Math.floor(Math.random() * arr.length));
+            let removed = deck.splice(index, 1);
+            shuffledDeck.push(removed);
+        };
+        shuffledDeck = shuffledDeck.flat();
+    };
+
+    shuffle(deck);
+    console.log(deck);
+    console.log(shuffledDeck);
+
+//--DEAL THE DECK
+
+function dealDeck(arr) {
+     playerOneDeck = arr.slice(0,(arr.length/2));
+     playerTwoDeck = arr.slice(arr.length/2,arr.length);
     }
-Game.prototype.compare = function() {
-    player1card > player2card ? player1score + 1: player2score +1;
-}
+
+//--DRAW
+dealDeck(shuffledDeck);
+
+    console.log(playerOneDeck);
+    console.log(playerTwoDeck);
+
+    function draw() {
+        let playerOneCard = playerOneDeck.shift();
+        let playerTwoCard = playerTwoDeck.shift();
+        console.log(playerOneCard);
+        console.log(playerTwoCard);
+        console.log(playerOneCard.value);
+        console.log(playerTwoCard.value);
+        if (playerOneCard.value > playerTwoCard.value) {
+            playerOneDeck.push(playerOneCard, playerTwoCard);
+        }
+    }
+    draw();
+
+    console.log(playerOneDeck);
+    console.log(playerTwoDeck);
 
 
-makeDeck = function() { 
-    for (let i = 2; i < 15; i++) {
-    deck.push(new Card(i,'heart'));
-    deck.push(new Card(i,'spade'));
-    deck.push(new Card(i,'diamond'));
-    deck.push(new Card(i,'club'));
-};
-};
-makeDeck();
+//EXTRA CODE, unsure if any will be used
+    // function Game(player1card, player2card, player1score = 0, player2score = 0) {
+    //     this.player1card = player1card
+    //     this.player2card = player2card
+    //     this.player1score = player1score
+    //     this.player2score = player2score
+    // }
+    // Game.prototype.compare = function() {
+    //     player1card > player2card ? player1score + 1: player2score +1;
+    // }
 
-
-
-
-
-
-
-
-// const Deck = function () {};
-
-// Deck.prototype.makeDeck = function() { 
-//             for (let i = 2; i < 15; i++) {
-//             deck.push(new Card(i,'heart'));
-//             deck.push(new Card(i,'spade'));
-//             deck.push(new Card(i,'diamond'));
-//             deck.push(new Card(i,'club'));
-//         }
-//         console.log(deck);
-// };
-
-//  Deck.prototype.shuffle = function(){
-//      console.log(`this is a test`)
-// };
-
-// const anotherDeck = new Deck
-// anotherDeck.makeDeck();
-// anotherDeck.shuffle();
-
-// function Player(name, deck = 0, totalcards = 0) {
-//     this.name = name
-//     this.deck = deck
-//     this.totalcards = totalcards
-// }
+    // function Player(name, deck = 0, totalcards = 0) {
+    //     this.name = name
+    //     this.deck = deck
+    //     this.totalcards = totalcards
+    // }
 
 
 

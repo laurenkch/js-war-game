a5ed4acd78612c6f99646bc5ee3a208b4949911d
laurenkch <lkoch879@gmail.com>
2022-01-15 14:09:09 -0500

    breaking down draw function into smaller functions

--- scripts/main.js
+++ scripts/main.js
@@ -36,9 +36,6 @@
         }
     };
 
-    // let player1 = new Player('you');
-    // let player2 = new Player('computer');
-
     //SHUFFLES YOUR WIN CARDS WHEN YOU RUN OUT IN YOUR DECK
 
     Player.prototype.reset = function () {
@@ -107,10 +104,8 @@
         console.log(`good to go`);
         console.log(currentGame.player1);
         console.log(currentGame.player2);
-        drawButton.addEventListener('click', this.draw);
+        // drawButton.addEventListener('click', this.draw);
     };
-
-
 
     //-----checks to see if you need to reshuffle the win pile or not and then draws 1 card. 
 
@@ -123,25 +118,45 @@
 
     Player.prototype.hasmoreCards = function() {
        let results = this.totalRemainingCards() > 0;
-        console.log(results)
         return results;
-    }
+    };
+
+    Game.prototype.readDrawResults = function() {
+        console.log(`\nYou drew the ${this.player1.drawnCards[0].value} of ${this.player1.drawnCards[0].suit}s`);
+        console.log(`The computer drew the ${this.player2.drawnCards[0].value} of ${this.player2.drawnCards[0].suit}s`);
+    };
+
+    Game.prototype.compare = function() {
+     if (this.player1.drawnCards[0].value > this.player2.drawnCards[0].value) {
+         return `You win!`
+        } else if (this.player1.drawnCards[0].value < this.player2.drawnCards[0].value) {
+         return `You lose :(`
+        } else {
+         return 'ITS WAR'
+        }
+     };
+
+     Player.prototype.canGoToWar = function() {
+        return this.totalRemainingCards() >= 4
+     };
+
+     Game.prototype.updateOverallResults = function () {
+        console.log(`player 1 cards in play ${this.player1.drawnCards.length}`);
+        console.log(`player 2 cards in play ${this.player2.drawnCards.length}`);
+        console.log(`Player1 total remaining cards: ${this.player1.totalRemainingCards()}`);
+        console.log(`Player2 total remaining cards: ${this.player2.totalRemainingCards()}`);
+};
+
 
     Game.prototype.draw = function() {
 
         this.player1.drawOne();
         this.player2.drawOne();
 
-        //-------tells you want you drew
+        this.readDrawResults();
 
-        console.log(`\nYou drew the ${this.player1.drawnCards[0].value} of ${this.player1.drawnCards[0].suit}s`);
-        console.log(`The computer drew the ${this.player2.drawnCards[0].value} of ${this.player2.drawnCards[0].suit}s`);
-        
-
-        //------compares the values of the drawn cards. 
-
-        if (this.player1.drawnCards[0].value > this.player2.drawnCards[0].value) {
-            if(this.player2.hasmoreCards === false) {
+        if (this.compare() === 'You win!') {
+            if(this.player2.hasmoreCards() === false) {
                 console.log(`!!!!!!You win the game!!!!!!`);
             } else {
             console.log(`You win!`)
@@ -149,45 +164,71 @@
             this.player1.drawnCards = [];
             this.player2.drawnCards = [];
             };
-        } else if (this.player1.drawnCards[0].value < this.player2.drawnCards[0].value) {
-            if(this.player1.hasmoreCards === false) {
+        } else if (this.compare() === 'You lose :(') {
+            if(this.player1.hasmoreCards() === false) {
                 console.log(`You lose the game :((((((`)
             } else {
             console.log(`You lose :(`)
             this.player2.winPile.push(...this.player2.drawnCards.concat(this.player1.drawnCards));
             this.player1.drawnCards = [];
-            this.player2.drawnCards = [];  
-            };    
-        } else if (this.player1.drawnCards[0].value === this.player1.drawnCards[0].value){
-            if (this.player1.totalRemainingCards() >= 4 && this.player2.totalRemainingCards() >= 4) {
-            console.log('ITS WAR')
-            this.call(currentGame.player1);
-            this.call(currentGame.player2);
-            this.call(currentGame.player1);
-            this.call(currentGame.player2);
-            this.call(currentGame.player1);
-            this.call(currentGame.player2);
-            } else if (this.player1.totalRemainingCards() < 4) {
-                console.log(`You lose the game :((((((`);
-            } else if (this.player2.totalRemainingCards() < 4) {
-                console.log(`You win the game!!!!!`);
-        };
+            this.player2.drawnCards = [];
+            };
+        } else {
+            if (this.player1.canGoToWar() && this.player2.canGoToWar()) {
+                console.log('ITS WAR')
+                this.player1.drawOne();
+                this.player2.drawOne();
+                this.player1.drawOne();
+                this.player2.drawOne();
+                this.player1.drawOne();
+                this.player2.drawOne();
+                } else if (this.player1.canGoToWar() === false && this.player2.canGoToWar()) {
+                    console.log(`You can't go to war. You lose the game :((((((`);
+                } else if (this.player2.canGoToWar() === false && this.player1.canGoToWar()) {
+                    console.log(`Computer can't go to war. You win the game!!!!!`);
+                } else {
+                    console.log(`No one can go to war, it's a draw`)
+                };
+        }
+        this.updateOverallResults();
     };
-        //logs the totals for both players
 
-        console.log(`player 1 cards in play ${this.player1.drawnCards.length}`);
-        console.log(`player 2 cards in play ${this.player2.drawnCards.length}`);
-        console.log(`Player1 total remaining cards: ${this.player1.totalRemainingCards()}`);
-        console.log(`Player2 total remaining cards: ${this.player2.totalRemainingCards()}`);
-};
 
 function drawBothPlayers() {
     currentGame.draw();
 };
 
 
-
-
-
-
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// at some point I had code like this that seemed to work but I wanted to save it for a question:
+
+// } else if (this.player1.drawnCards[0].value === this.player1.drawnCards[0].value){
+//     if (this.player1.totalRemainingCards() >= 4 && this.player2.totalRemainingCards() >= 4) {
+//     console.log('ITS WAR')
+//     this.call(currentGame.player1);
+//     this.call(currentGame.player2);
+//     this.call(currentGame.player1);
+//     this.call(currentGame.player2);
+//     this.call(currentGame.player1);
+//     this.call(currentGame.player2);
+//     } else if (this.player1.totalRemainingCards() < 4) {
+//         console.log(`You lose the game :((((((`);
+//     } else if (this.player2.totalRemainingCards() < 4) {
+//         console.log(`You win the game!!!!!`);
+
+// but looking back on it, i'm not sure why it worked at all. "this.call" seems to deal out a new card 3 times each but doesn't reference the name of the function draw one. I guess this section of code was nested in the overall function that did deal out one card each for each player, but it didn't run the whole funciton three times because it didn't return the winner and loser of the intermediate cards. Maybe it sort of made a recursive function??
